4baea60ce05c45742f23f118b84760b9d1db5902
seagazer <seagazer@qq.com>
2022-09-26 21:28:13 +0800

    [dev]更新demo，增加标题栏示例

--- entry/src/main/ets/MainAbility/MainAbility.ts
+++ entry/src/main/ets/MainAbility/MainAbility.ts
@@ -3,7 +3,6 @@ import Ability from '@ohos.application.Ability'
 export default class MainAbility extends Ability {
     onCreate(want, launchParam) {
         globalThis.abilityWant = want;
-        globalThis.context = this.context
     }
 
     onWindowStageCreate(windowStage) {
