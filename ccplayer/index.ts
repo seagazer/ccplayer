4e1a562e34c33d266cc3142769e85f9c1892d529
seagazer <seagazer@qq.com>
2022-11-12 11:44:53 +0800

    [dev]修改mediasource可选参数，增加surface销毁回调，更新示例

--- ccplayer/index.ts
+++ ccplayer/index.ts
@@ -1,3 +1,5 @@
+export { Logger } from './src/main/ets/player/common/Logger'
+
 export { CcPlayer } from './src/main/ets/player/CcPlayer'
 
 export { PlayerType } from './src/main/ets/player/config/PlayerType'
@@ -7,8 +9,6 @@ export { PlayerState } from './src/main/ets/player/config/PlayerState'
 export { MediaSourceFactory } from './src/main/ets/player/data/MediaSourceFactory'
 
 export { MediaSource } from './src/main/ets/player/data/MediaSource'
-
-export { Logger } from './src/main/ets/player/common/Logger'
 
 export { AspectRatio } from './src/main/ets/player/config/AspectRatio'
 
--- entry/src/main/ets/MainAbility/MainAbility.ts
+++ entry/src/main/ets/MainAbility/MainAbility.ts
@@ -1,4 +1,7 @@
+import { Logger } from '@seagazer/ccplayer';
 import Ability from '@ohos.application.Ability'
+
+const TAG = "MainAbility"
 
 export default class MainAbility extends Ability {
     onCreate(want, launchParam) {
@@ -7,5 +10,24 @@ export default class MainAbility extends Ability {
 
     onWindowStageCreate(windowStage) {
         windowStage.setUIContent(this.context, "pages/main", null)
+        this.requestPermission()
+    }
+
+    private requestPermission() {
+        let permissions: Array<string> = [
+            "ohos.permission.READ_MEDIA",
+            "ohos.permission.WRITE_MEDIA",
+            "ohos.permission.READ_USER_STORAGE",
+            "ohos.permission.WRITE_USER_STORAGE"
+        ]
+        this.context.requestPermissionsFromUser(permissions, (err, result) => {
+            if (err) {
+                Logger.d(TAG, 'requestPermissionsFromUserError: ' + JSON.stringify(err));
+
+            } else {
+                let permissionRequestResult = result;
+                Logger.d(TAG, 'permissionRequestResult: ' + JSON.stringify(permissionRequestResult));
+            }
+        })
     }
 };
