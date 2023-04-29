f8c505de169d0b1f0b7687039f59d7d2979f0b3b
seagazer <seagazer@qq.com>
2023-04-29 13:01:37 +0800

    [dev]更新demo示例工程，适配OpenHarmony3.2

--- entry/src/main/ets/MainAbility/MainAbility.ts
+++ entry/src/main/ets/MainAbility/MainAbility.ts
@@ -1,17 +1,13 @@
 import UIAbility from '@ohos.app.ability.UIAbility';
 import { Logger } from '@seagazer/ccplayer';
 
-const TAG = "MainAbility"
-
 export default class MainAbility extends UIAbility {
     onCreate(want, launchParam) {
         globalThis.abilityWant = want;
     }
 
     onWindowStageCreate(windowStage) {
-        windowStage.setUIContent(this.context, "pages/main", null)
-        this.requestPermission()
+        windowStage.setUIContent(this.context, "pages/logo", null)
     }
 
-
-};
+}
