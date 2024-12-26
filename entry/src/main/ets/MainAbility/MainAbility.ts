6393f32a827b13a099bca4dcb43da25465d0f687
seagazer <seagazer@qq.com>
2024-12-26 21:32:38 +0800

    [dev]增加默认手势和加载Overlay组件

--- entry/src/main/ets/MainAbility/MainAbility.ts
+++
@@ -1,8 +0,0 @@
-import UIAbility from '@ohos.app.ability.UIAbility';
-import window from '@ohos.window';
-
-export default class MainAbility extends UIAbility {
-    onWindowStageCreate(windowStage: window.WindowStage): void {
-        windowStage.loadContent("pages/Logo")
-    }
-}
